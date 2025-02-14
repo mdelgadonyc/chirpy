ce12c6aa0794245c059f70afebe73c3270c99b1a
mdelgadonyc <moises.f.delgado@gmail.com>
2025-02-13 23:01:47 -0500

    Add health check endpoint and update file server path handling

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", healthReadiness)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -20,4 +21,10 @@ func main() {
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
 
+}
+
+func healthReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
